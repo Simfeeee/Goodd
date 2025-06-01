0bcd1f610c67b71b57c5e4ea2a91757b7d532016
Simfeeee <toxaslyadnev@gmail.com>
2025-06-01 19:42:17 +0300

    Add files via upload

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,12 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from bot import send_latest_news, send_evening_digest
+from tasks import send_latest_news, send_evening_digest
+from config import TELEGRAM_BOT_TOKEN
+from aiogram import Bot
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 def start_scheduler():
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_latest_news, "interval", minutes=30)
-    scheduler.add_job(send_evening_digest, "cron", hour=21, minute=0)
+    scheduler.add_job(send_latest_news, "interval", minutes=30, args=[bot])
+    scheduler.add_job(send_evening_digest, "cron", hour=21, minute=0, args=[bot])
     scheduler.start()
